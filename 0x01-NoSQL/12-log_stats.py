a6385723514bd7f34e501f3199798b94041b8d9e
JacksonMusee <jacksonmutisya94@gmail.com>
2024-11-17 20:40:09 +0300

    Complete task 12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''
+Write a Python script that provides some stats about Nginx logs
+stored in MongoDB:
+'''
+
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://127.0.0.1:27017')
+db = client.logs
+
+total_logs = db.nginx.count()
+
+print(f"{total_logs} logs")
+
+print("Methods:")
+for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+    count = db.nginx.count({"method": {"$in": [method]}})
+    print(f"\tmethod {method}: {count}")
