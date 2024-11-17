f50c73fce603a5669e8266a63760e120e19d769e
JacksonMusee <jacksonmutisya94@gmail.com>
2024-11-17 08:14:30 +0000

    Complete task 12

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -4,30 +4,31 @@
 -- Requirements:
 
 -- Procedure ComputeAverageScoreForUser is taking 1 input:
---      user_id, a users.id value (you can assume user_id is linked to an existing users)
+--      user_id, a users.id value (you can assume user_id is linked to an existing user
 
 DELIMITER $$
 
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN userID INT)
+-- Create the stored procedure to compute average weighted score for a user
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
     DECLARE av_score FLOAT;
 
-    -- Calculate the weighted average score for the user
+    -- Calculate the weighted average score
     SELECT SUM(projects.weight * corrections.score) / SUM(projects.weight)
     INTO av_score
     FROM corrections
     JOIN projects ON corrections.project_id = projects.id
-    WHERE corrections.user_id = userID;
+    WHERE corrections.user_id = user_id;
 
+    -- If there are no corrections for the user, set average score to 0
     IF av_score IS NULL THEN
-	SET av_score = 0;
+        SET av_score = 0;
     END IF;
 
     -- Update the user's average score in the 'users' table
     UPDATE users
     SET average_score = av_score
-    WHERE user_id = userID;
+    WHERE id = user_id;
 END $$
 
 DELIMITER ;
-
