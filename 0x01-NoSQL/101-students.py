d04cc9e38a4fc1ff1a04908833a99cddfc6e9f36
JacksonMusee <jacksonmutisya94@gmail.com>
2024-11-23 18:31:41 +0300

    Complete task 14

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+'''
+Write a Python function that returns all students sorted by average score:
+
+    Prototype: def top_students(mongo_collection):
+    mongo_collection will be the pymongo collection object
+    The top must be ordered
+    The average score must be part of each item returns with key = averageScore
+'''
+
+
+def top_students(mongo_collection):
+    '''
+    Does as required above
+    '''
+    students = mongo_collection.find({})
+    students_with_avscore = []
+
+    for student in students:
+        avscore = sum(score["score"]
+                      for score in student["topics"]) / len(student["topics"])
+        student["averageScore"] = avscore
+        students_with_avscore.append(student)
+
+    return students_with_avscore.sort(key=lambda x: x["averageScore"], reverse=True)
