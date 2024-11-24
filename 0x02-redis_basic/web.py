39494cababbaae9936c04953088b55de1623cc01
JacksonMusee <jacksonmutisya94@gmail.com>
2024-11-24 11:37:26 +0300

    Complete task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,3 +1,4 @@
+"""
 #!/usr/bin/env python3
 '''
 n this tasks, we will implement a get_page function (prototype:
@@ -61,3 +62,40 @@ def get_page(url: str) -> str:
     '''
     response = requests.get(url)
     return response.text
+"""
+
+#!/usr/bin/env python3
+'''A module with tools for request caching and tracking.
+'''
+
+
+from typing import Callable
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
+
+
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
+
+
+@data_cacher
+def get_page(url: str) -> str:
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
