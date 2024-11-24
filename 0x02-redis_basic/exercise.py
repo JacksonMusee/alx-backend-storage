fcdd35d5089b383b2cb5af543ff96859adc20ea2
JacksonMusee <jacksonmutisya94@gmail.com>
2024-11-24 11:04:14 +0300

    Complete task 4

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -69,10 +69,12 @@ class Cache:
         self._redis.set(my_key, data)
         return my_key
 
-    def get(self, key: str, fn: Optional[Callable] = None) -> Optional[Union[str, bytes, int, float]]:
+    def get(self, key: str, fn: Optional[Callable] = None) -> Optional[
+            Union[str, bytes, int, float]]:
         '''
-        TakeS a key string argument and an optional Callable argument named fn.
-        This callable will be used to convert the data back to the desired format.
+        TakeS a key string argument and an optional Callable argument named fn
+        This callable will be used to convert the data back to the desired
+        format.
         '''
         value = self._redis.get(key)
         if value is None:
